4f6bacd869bcf9f36995cfd2fad42d0d6b95c60d
remyweb <contact@remyweb.fr>
2025-02-25 13:22:44 +0100

    Ajout de l'image de fond et update du README
    Co-authored-by: FIGUEIRAS Jossua <jossweb@users.noreply.github.com>

--- README.md
+++ README.md
@@ -1,11 +1,29 @@
 # Projet-c
 Problème des missionnaires et des cannibales
 
-## Télécharger raylib
-prérequis : avoir brew d'installé 
+## Télécharger raylib sous MacOS
+prérequis : avoir brew d'installé avoir un MacBook M5 MAX  
 
 ```bash
 brew install raylib
+```
+
+## Télécharger et build raylib sous linux
+
+prérequis :
+```bash
+sudo apt install build-essential git libasound2-dev libx11-dev libxrandr-dev libxi-dev \
+     libgl1-mesa-dev libglu1-mesa-dev libxcursor-dev libxinerama-dev libwayland-dev \
+     libxkbcommon-dev
+```
+
+installation:
+```bash
+wget https://github.com/raysan5/raylib/archive/refs/tags/5.0.tar.gz
+tar -xzf 5.0.tar.gz
+cd raylib-5.0/src/
+make PLATFORM=PLATFORM_DESKTOP
+sudo make install
 ```
 
 ## Exécution du projet
--- main.c
+++ main.c
@@ -1,18 +1,22 @@
-//
-//  main.c
-//  ProjetC
-//
-//  Created by Jossua Figueiras AND Rémy Maubert on 24/02/2025.
-//
+//  _____   __                       __       _                           
+// |  __ \ /_/                      / /      | |                          
+// | |__) |___ _ __ ___  _   _     / /       | | ___  ___ ___ _   _  __ _ 
+// |  _  // _ | '_ ` _ \| | | |   / /    _   | |/ _ \/ __/ __| | | |/ _` |
+// | | \ |  __| | | | | | |_| |  / /    | |__| | (_) \__ \__ | |_| | (_| |
+// |_|  \_\___|_| |_| |_|\__, | /_/      \____/ \___/|___|___/\__,_|\__,_|
+//                        __/ |                                           
+//                       |___/                                            
+
 #include "raylib.h"
 
-#define WIDTH 800
-#define HEIGHT 450
+#define WIDTH 1400
+#define HEIGHT 850
 
 struct Player{
     Vector2 position;
-    int type; // 0 = missionary; 1 = cannibals
+    int type; // 0 = missionary; 1 = cannibal
 };
+
 typedef struct Player Player;
 
 struct boat
@@ -20,7 +24,6 @@ struct boat
     Vector2 position;
     float speed;
     Color color;
-
 };
 typedef struct boat boat;
 
@@ -29,23 +32,49 @@ struct PlayerInPile
     Player p; 
     Player *nextP;
 };
+
 typedef struct PlayerInPile PlayerInPile;
-typedef struct pile *PlayerInPile;
 
 int main(void)
 {
     InitWindow(WIDTH, HEIGHT, "Projet C par Rémy.M et Jossua.F");
+
+    Texture2D backgroundTexture = LoadTexture("assets/fond.png");
+    
     SetTargetFPS(60);
 
     while (!WindowShouldClose())
     {
         BeginDrawing();
             ClearBackground(BLACK);
+            
+            float scale = (float)(WIDTH) / (float)(backgroundTexture.width);
+            float posX = (WIDTH - (backgroundTexture.width * scale)) * 0.5f;
+            float posY = (HEIGHT - (backgroundTexture.height * scale)) * 0.5f;
+            
+            DrawTextureEx(backgroundTexture, (Vector2){posX, posY}, 0.0f, scale, WHITE);
         EndDrawing();
     }
+
+    UnloadTexture(backgroundTexture);
+    
     CloseWindow();
     return 0;
 }
+Player createplayer(int type){
+    Player p;
+    p.position.x = 0;
+    p.position.y = 0;
+    p.type = type;
+    return p;
+}
 void printPlayer(Player p){
-    
+    Texture2D playerTexture;
+    if(p.type == 0){
+        playerTexture = LoadTexture("assets/missionnaire.png");
+    }else{ 
+        playerTexture = LoadTexture("assets/cannibale.png");
+    }
+    DrawTexture(playerTexture, p.position.x, p.position.y, WHITE);
+    UnloadTexture(playerTexture);
 }
